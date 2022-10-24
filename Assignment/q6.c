3c334650311660525bf09c1166dfd1e699d7ef93
Sehan Arandara <99310968+SehanArandara@users.noreply.github.com>
2022-10-24 23:36:23 +0530

    Update q6.c

--- Assignment/q6.c
+++ Assignment/q6.c
@@ -10,7 +10,6 @@ void * thread1()
         for(int k=2;k<102;k+=2){
           printf("%d\n",k);
         }
-
         pthread_exit(0);
 }
 int main()
@@ -18,14 +17,12 @@ int main()
         pthread_t tid[2];   // thread variable
         int i;
         int n;
-
+        
         printf("enter an integer :");
         scanf("%d",&n);
-
-      
+        
         for(i=0;i<NoOfTrd;i++){
           pthread_create(&tid[i],NULL,thread1,NULL);
-          
         }
         for(i=0;i<NoOfTrd;i++){
           pthread_join(tid[i],NULL);
@@ -33,8 +30,5 @@ int main()
         for(i=0;i<=n;i++){
           printf("%d\n",i);
         }
-        
-            
-        
         return 0;
 }
